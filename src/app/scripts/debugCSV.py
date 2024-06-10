7e8cbd6fda129464d4ad95e6b5c0fe1bc2ca8823
Valfonsoardila10 <victoradila@gmail.com>
2024-06-09 21:57:39 -0500

    Cambios para prueba

--- src/app/scripts/debugCSV.py
+++ src/app/scripts/debugCSV.py
@@ -23,7 +23,7 @@ def debug(dataframe, path):
                 operatingSystem = platformsSys.get_operatingSystem()
                 ruta_exe = (ruta_dos_niveles_arriba / 'app' / 'exe' / 'windows' / 'menuDebug.bat') if operatingSystem == "Windows" else (ruta_dos_niveles_arriba / 'app' / 'exe' / 'linux' / 'menuDebug.sh')
                 # Ejecutar el archivo .bat y ejecutar como administrador
-                proceso = subprocess.Popen(['start', 'cmd', '/k', 'call', str(ruta_exe)], shell=True)
+                proceso = subprocess.Popen(['start', 'cmd', 'call', str(ruta_exe)], shell=True)
                 # Obtener la salida del proceso .bat
                 salida_bytes, _ = proceso.communicate()
                 # Decodificar la salida del proceso .bat
