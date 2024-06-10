66379fc3a1ae1d8591b32e8b9f4f7aae17461fc5
Valfonsoardila10 <victoradila@gmail.com>
2024-06-10 00:16:35 -0500

    Funcional

--- src/app/scripts/debugCSV.py
+++ src/app/scripts/debugCSV.py
@@ -29,7 +29,7 @@ def debug(dataframe, path):
 
                 # Ejecutar el archivo .bat o .sh de forma síncrona y esperar a que termine
                 if operatingSystem == "Windows":
-                    proceso = subprocess.run(['cmd', '/c', 'start', '/B',  str(ruta_exe)], shell=True)
+                    proceso = subprocess.run(['cmd', '/c', 'start',  str(ruta_exe)], shell=True)
                 else:
                     proceso = subprocess.Popen(['gnome-terminal', '--', str(ruta_exe)], shell=False)
                 
